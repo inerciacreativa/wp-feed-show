b3cdd2916010599dcca473002eaa6f6ddef005c3
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2019-03-03 07:00:37 +0100

    Updated version

--- feed-show.php
+++ feed-show.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ic Feed Show
  * Plugin URI:  https://github.com/inerciacreativa/wp-feed-show
- * Version:     2.0.2
+ * Version:     2.0.3
  * Text Domain: ic-feed-show
  * Domain Path: /languages
  * Description: Widget para mostrar feeds RSS.
@@ -16,4 +16,16 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-ic\Plugin\FeedShow\FeedShow::create(__FILE__, WP_PLUGIN_DIR);
+if (!class_exists(ic\Framework\Framework::class)) {
+	trigger_error('ic Framework not found', E_USER_ERROR);
+}
+
+if (!class_exists(ic\Plugin\RewriteControl\RewriteControl::class)) {
+	if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+		include_once __DIR__ . '/vendor/autoload.php';
+	} else {
+		trigger_error('Could not load RewriteControl class', E_USER_ERROR);
+	}
+}
+
+ic\Plugin\FeedShow\FeedShow::create(__FILE__);
