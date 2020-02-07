37bd722bb7aaa41ffb0d99f325936526a47c0f6a
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2020-02-07 11:37:12 +0100

    Updated major version (use ic Framework 5.0)

--- feed-show.php
+++ feed-show.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ic Feed Show
  * Plugin URI:  https://github.com/inerciacreativa/wp-feed-show
- * Version:     4.0.1
+ * Version:     5.0.0
  * Text Domain: ic-feed-show
  * Domain Path: /languages
  * Description: Widget para mostrar feeds RSS.
